922f20da971b2df33cc1d2d41ddfc38389cb7daa
Mike <wade88081502@gmail.com>
2021-03-28 22:45:14 +0800

    hw4

---
+++ JavaScriptExercise/FirstJavaScriptApp/Test.js
@@ -0,0 +1,18 @@
+var username = "Mary";
+
+{
+    var username = "Kevin";
+    console.log(username)
+}
+
+console.log(username)
+
+
+let username = "Mary";
+
+{
+    let username = "Kevin";
+    console.log(username)
+}
+
+console.log(username)
